d33ab8098ede8be03a5e403b111a041166ee2578
Aishamos <you_1699@foxmail.com>
2026-07-31 14:18:53 +0800

    OUR GALLERY与返回首页增加最小间距，防止文字换行

--- src/pages/UploadPage.vue
+++ src/pages/UploadPage.vue
@@ -5,9 +5,9 @@
       v-if="!authed"
       class="h-full flex flex-col"
     >
-      <div class="max-w-7xl mx-auto px-6 py-5 mb-6 flex justify-between items-center">
-        <div class="text-xl tracking-widest">OUR GALLERY</div>
-        <RouterLink to="/" class="text-sm text-gray-400 hover:text-gray-700 transition-colors">返回首页</RouterLink>
+      <div class="max-w-7xl mx-auto px-6 py-5 flex justify-between items-center gap-6">
+        <div class="text-xl tracking-widest whitespace-nowrap">OUR GALLERY</div>
+        <RouterLink to="/" class="text-sm text-gray-400 hover:text-gray-700 transition-colors whitespace-nowrap">返回首页</RouterLink>
       </div>
 
       <div class="flex-1 flex items-center justify-center px-5">
@@ -51,10 +51,10 @@
     <!-- 上传界面 -->
     <div v-else class="max-w-7xl mx-auto px-6 py-5">
       <!-- 顶栏 -->
-      <div class="flex justify-between items-center mb-12">
-        <div class="text-xl tracking-widest">OUR GALLERY</div>
+      <div class="flex justify-between items-center mb-12 gap-6">
+        <div class="text-xl tracking-widest whitespace-nowrap">OUR GALLERY</div>
         <button
-          class="text-sm text-gray-400 hover:text-gray-700 transition-colors"
+          class="text-sm text-gray-400 hover:text-gray-700 transition-colors whitespace-nowrap"
           @click="goHome"
         >
           返回首页
